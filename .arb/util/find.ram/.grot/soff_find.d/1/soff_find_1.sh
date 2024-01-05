1b21741a7b1b3f1117dd4085ff994141617b7d6b
[alt]legioner9 <legioner9@inbox.ru>
2024-01-05 19:32:49 +0700

    <> ##

--- .arb/util/find.ram/.grot/soff_find.d/1/soff_find_1.sh
+++ .arb/util/find.ram/.grot/soff_find.d/1/soff_find_1.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+cd ${REPO_PATH}/NBash/.arb/util/find.ram/.grot/soff_find.d/1
+
+ehh find
+ 
+ehh find ${REPO_PATH}/NBash/.arb/util/find.ram/.grot/soff_find.d/1 -printf "%f\n" |  grep '^[^_].*'
+
+ehh find ${REPO_PATH}/NBash/.arb/util/find.ram/.grot/soff_find.d/1 -regex '^[^_].*'
+
+
