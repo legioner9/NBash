80889942854ec6512da5caadcb5da9117269d26e
[alt]legioner9 <legioner9@inbox.ru>
2024-01-15 16:12:36 +0700

    <> soff_find #2

--- .arb/util/find.ram/.grot/soff_find.d/2/soff_find_1.sh
+++ .arb/util/find.ram/.grot/soff_find.d/2/soff_find_1.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+echo -e "${HLIGHT}--- find /home/st/REPOBARE/_repo/tgu -type f -size +1M -not -path \"/home/st/REPOBARE/_repo/tgu/.git/*\" ---${NORMAL}" #start files
+find /home/st/REPOBARE/_repo/tgu -type f -size +1M -not -path "/home/st/REPOBARE/_repo/tgu/.git/*"
+
+if ! find /home/st/REPOBARE/_repo/tgu -type f -size +1M -not -path "/home/st/REPOBARE/_repo/tgu/.git/*"; then
+    echo FALSE 0
+else
+    echo TRUE 0
+fi
+
+if find /home/st/REPOBARE/_repo/tgu -type f -size +1M -not -path "/home/st/REPOBARE/_repo/tgu/.git/*"; then
+    echo TRUE 1
+else
+    echo FALSE 1
+fi
+
+echo -e "${HLIGHT}--- find /home/st/REPOBARE/_repo/tgu -type f -size +45M -not -path \"/home/st/REPOBARE/_repo/tgu/.git/*\" ---${NORMAL}" #start files
+find /home/st/REPOBARE/_repo/tgu -type f -size +45M -not -path "/home/st/REPOBARE/_repo/tgu/.git/*"
+
+if ! find /home/st/REPOBARE/_repo/tgu -type f -size +45M -not -path "/home/st/REPOBARE/_repo/tgu/.git/*"; then
+    echo FALSE 2
+else
+    echo TRUE 2
+fi
+
+if find /home/st/REPOBARE/_repo/tgu -type f -size +45M -not -path "/home/st/REPOBARE/_repo/tgu/.git/*"; then
+    echo TRUE 3
+else
+    echo FALSE 3
+fi
