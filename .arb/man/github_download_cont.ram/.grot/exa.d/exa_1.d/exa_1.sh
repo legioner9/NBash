a0d4d6f57b5e9b4c176053557c67d3cd1ff72c46
[alt]legioner9 <legioner9@inbox.ru>
2024-02-24 17:28:23 +0700

    <> ##

---
+++ .arb/man/github_download_cont.ram/.grot/exa.d/exa_1.d/exa_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd ${REPO_PATH}/NBash/.arb/man/github_download_cont.ram/.grot/exa.d/exa_1.d || plt_exit
+
+# bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
+c#! url -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh > mhsanaei_install.sh
+
+# https://github.com/legioner9/lex/tree/master/.d/.read
+
+#! wget --no-parent -r https://raw.githubusercontent.com/legioner9/lex/master/.d/.read/date.man
+# wget --no-parent -r https://raw.githubusercontent.com/legioner9/lex/master/.d/.read/*
+
+# wget --no-parent -r https://raw.githubusercontent.com/legioner9/lex/master/.d/.read
+
+# wget --no-parent -r https://raw.gitfilcusercontent.com/legioner9/lex/master/.d/.read/date.man
+
+
