599bddb47c8c2691ac19e676bd509378a14f4179
[alt]legioner9 <legioner9@inbox.ru>
2024-03-01 12:24:38 +0700

    <> ##

--- .arb/util/docker.ram/.grot/game_badtry/exa.d/exa_1.d/exa_1.sh
+++ .arb/util/docker.ram/.grot/game_badtry/exa.d/exa_1.d/exa_1.sh
@@ -3,4 +3,8 @@
 cd ${PATH_CURR_REPO}/NBash/.arb/util/docker.ram/.grot/game_badtry/exa.d/exa_1.d || plt_exit
 
 # docker pull ubuntu:18.10
-docker images
+${_ehh} docker images
+# ${_ehh} docker run ubuntu echo 'hello from ubuntu' 
+${_ehh} docker ps
+${_ehh} docker ps -a
+# ${_ehh} docker run -it ubuntu:18.10 /bin/bash
