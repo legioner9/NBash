4b3a88eccbe763391dff906442759ca43a71434c
[alt]legioner9 <legioner9@inbox.ru>
2024-03-02 13:33:39 +0700

    <> ##

---
+++ .arb/man/read_stdin.ram/.grot/exa.d/exa_1.d/echo1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+n=0
+while IFS=$'\n' read -r line; do
+    echo "$n: $line"
+    n=$((n+1))
+done </dev/stdin
---
+++ .arb/man/read_stdin.ram/.grot/exa.d/exa_1.d/exa_1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ${REPO_PATH}/NBash/.arb/man/read_stdin.ram/.grot/exa.d/exa_1.d || plt_exit
+
+dk_pa__ | . echo2.sh
+
+cat > txt.file <<EOF
+weqf
+cwq
+cwqc
+EOF
+
+. echo2.sh txt.file
+
+dk_pa__ | . echo1.sh
