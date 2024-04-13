ac1ca4c32a11e6efb3acc5a59a83096d13681b23
[alt]legioner9 <legioner9@inbox.ru>
2024-04-13 21:56:46 +0700

    <> ##

--- .arb/man/str_to_arr.ram copy/.grot/exa.d/exa_1.d/2_with_arg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# str="asfdf ewrf::werfer ewrfgwre"
-# IFS=$'^'
-# arr_str=(${str//::/^})
-# IFS=$' \t\n'
-# _parr3e arr_str
---
+++ .arb/man/str_to_arr.ram/.grot/exa.d/exa_1.d/2_with_arg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# str="asfdf ewrf::werfer ewrfgwre"
+# IFS=$'^'
+# arr_str=(${str//::/^})
+# IFS=$' \t\n'
+# _parr3e arr_str
+
+# str="asfdf ewrf::werfer ewrfgwre"
+# IFS=$'^'
+# arr_str=(${str//::/^})
+# IFS=$' \t\n'
+# _parr3e arr_str
+#![[str_to_arr]]
