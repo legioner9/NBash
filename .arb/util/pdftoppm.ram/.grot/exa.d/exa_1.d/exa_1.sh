5510b5d23a960fda5baa4066d3ef2bd98d09f616
[alt]legioner9 <legioner9@inbox.ru>
2024-08-07 15:40:09 +0700

    <> ##

---
+++ .arb/util/pdftoppm.ram/.grot/exa.d/exa_1.d/exa_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir=${HOME}/REPOBARE/_repo/NBash/.arb/util/pdftoppm.ram/.grot/exa.d/exa_1.d
+
+cd ${dir} || echo "FAIL cd ${dir}" >&2
+
+
+cp "003.requ_1.pdf.pdf~" _tst/"003.requ_1.pdf.pdf"
+
+cd _tst || echo "FAIL cd _tst" >&2
